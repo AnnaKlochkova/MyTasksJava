fb06761be75685b68485f923f07b7a2f1180bcf9
Anna <a.n.klochkova1@gmail.com>
2017-01-20 20:49:20 +0200

    Task2
    Write a program that calculates and displays the area of a triangle if its sides are known.

---
+++ Exercise2.java
@@ -0,0 +1,16 @@
+package exercise2;
+
+public class Exercise2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double a=5.3;
+double b=4.1;
+double c=2.2;
+double p=(a+b+c)/2;
+double d=p*(p-a)*(p-b)*(p-c);
+double s = Math.sqrt(d);
+System.out.println("S =" +s);
+	}
+
+}
