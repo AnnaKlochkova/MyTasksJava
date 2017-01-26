991d92d30a7a6e10fac0e27a54d759aef478f392
Anna <a.n.klochkova1@gmail.com>
2017-01-26 21:44:52 +0200

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,40 @@
+package task1;
+
+import java.util.Scanner;
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a;
+		int b;
+		int c;
+		int d;
+		int max;
+		System.out.println("Input a");
+		a = sc.nextInt();
+		System.out.println("Input b");
+		b = sc.nextInt();
+		System.out.println("Input c");
+		c = sc.nextInt();
+		System.out.println("Input d");
+		d = sc.nextInt();
+		if (a > b) {
+			max = a;
+		}else{
+		 max = b;
+		}if (c > max)
+				   max = c;
+				if (d>max){
+					max=d;
+				}
+				System.out.println("Max =" + max);
+		}
+
+}
+
+
+
+
+
